6b68f574161ca19630823f114da1a6794942e253
Gennadiy Bakulin <g.bakulin@yandex.ru>
2024-11-08 19:23:04 +0700

    сгенерировал папку test

---
+++ src/test/java/com/javaacademy/burger/KitchenTest.java
@@ -0,0 +1,28 @@
+package com.javaacademy.burger;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class KitchenTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @AfterEach
+  void tearDown() {
+  }
+
+  @Test
+  void cook() {
+  }
+
+  @Test
+  void setHasGas() {
+  }
+
+  @Test
+  void getCompletedDishes() {
+  }
+}
